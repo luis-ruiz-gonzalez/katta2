5458fe517b013eacccc2ff48d7f44bd8cc72fa0b
Luis Ruiz <Luis Ruiz@192.168.1.39>
2020-01-22 19:40:42 +0000

    Cuarta versión de katta2

--- src/katta2/Katta2.java
+++ src/katta2/Katta2.java
@@ -1,13 +1,13 @@
 package katta2;
 
-import java.util.HashMap;
+
 import java.util.Map;
 
 public class Katta2 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        int data[] = {4,7,4,6,2,32,6,3,6,3,65,2,3,6,};
+        Integer data[] = {4,7,4,6,2,32,6,3,6,3,65,2,3,6,};
         Histogram histo = new Histogram(data);
         Map<Integer,Integer> histogr = histo.getHistogram();
         
