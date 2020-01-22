5ffa5fa95aa2c6b7721689746a9328aa60789f16
Luis Ruiz <Luis Ruiz@192.168.1.39>
2020-01-22 19:24:25 +0000

    Segunda versión de katta2

--- src/katta2/Katta2.java
+++ src/katta2/Katta2.java
@@ -10,12 +10,8 @@ public class Katta2 {
         int data[] = {4,7,4,6,2,32,6,3,6,3,65,2,3,6,};
         Map<Integer,Integer>  histogram = new HashMap<>();
         
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i]) == true){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }else{
-                histogram.put(data[i], 1);
-            }
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
 
         for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
